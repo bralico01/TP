49f283b545c4be8a782e1a642f7f489d9b282d56
kfoxone <raphaelhendrick@gmail.com>
2024-04-18 16:28:22 +0100

    Intégration partie 1 jeux de Guest Number
    A finaliser avec score

--- src/main/java/org/blc/training/tp/TP.java
+++ src/main/java/org/blc/training/tp/TP.java
@@ -11,52 +11,147 @@ import java.util.Scanner;
 public class TP {
 
     public static void main(String[] args) {
-        trouveLeNombreAleatoire();
-    }
-
-    public static void boucleWhileDo() {
-        Integer A = 500;
-        Integer B = 450;
-        Integer i = 1;
-
-        while (B < A) {
-            out(i + " : " + B + " < " + A + " : ");
-            B++; // incrémentation B de 1
-            i++;
-        }
-
-//        do {
-//
-//        } while (B < A);
+        trouveLeNombreAleatoireOptions();
     }
 
     /**
-     * 
-     * Options : chance 
-     *           avec limitation d'essai / 
-     *           Limiter le nombre d'essai simultanée ! 
-     * 
+     *
+     * Options : chance
+     * <p>
+     * avec limitation d'essai : On octroie en démarrant le jeu un nombre défini
+     * de chance et ce nombre n'est pas renouvlable entre les essais.
+     *
      * Options : Niveau difficulté
-     *          Niveau 0 - 0...10
-     *          Niveau 1 - 0...20
-     *          Niveau 2 - 0...30
-     *          ...
-     * 
-     * Options : Calculer le score de la personne
-     *          Nombre pour atteindre un niveau
-     * 
-     * 
+     * <p>
+     * Niveau 0 - 0...10
+     * <p>
+     * Niveau 1 - 0...20
+     * <p>
+     * Niveau 2 - 0...30 ...
+     *
+     * Options : Calculer le score de la personne Nombre pour atteindre un
+     * niveau
+     *
+     *
+     */
+    public static void trouveLeNombreAleatoireOptions() {
+        String yesno = "y";
+
+        /**
+         * Terminaison ou non de l'application
+         */
+        while (yesno.matches("y")) {
+            /**
+             * Niveau de difficulté logiciel
+             */
+            Integer level = 1;
+            Integer chance = 5;
+            Scanner scan = new Scanner(System.in);
+            Boolean isQuit = false;
+            while (!isQuit && chance > 0) {// Passage de niveau
+
+                outn("=|=====================================================|=");
+                outn("=| Niveau : " + level + " >> vous avez " + chance + " chances     |=");
+                outn("=|=====================================================|=");
+
+                Integer inconnu = (int) (Math.random() * (10 * level));
+                outn("Essayer de retrouver le nombre caché compris entre 0 et " + (level * 10) + " :");
+
+                Boolean isValueCorrect = false;
+                while (!isValueCorrect && !isQuit && chance > 0) { // Niveau en cours
+                    // Récupérer la valuer
+                    Integer value = -1;
+                    Boolean isCorrectInput = false;
+                    while (!isCorrectInput && !isQuit) { // Parce que les informatiosn saisie ne sont pas cohérente par rapport à un nombre ou à quitter
+                        out("Entrer un nombre de 0 à " + (10*level) + " : ");
+                        try {
+                            value = scan.nextInt();
+                            isCorrectInput = true;
+                        } catch (InputMismatchException erreur) {
+                            String str = scan.next();
+                            String msgError = "Vous devez renseigner un nombre ou \"q\" pour quitter";
+                            if (str != null) {
+                                if (str.toLowerCase().substring(0, 1).matches("q")) {
+                                    isQuit = true;
+                                } else {
+                                    outn(msgError);
+                                }
+                            } else {
+                                outn(msgError);
+                            }
+                        }
+                    }
+
+                    if (!isQuit) { // Only if user want to continue
+                        // Compare si le nombre fourni par l'utilisateur correspond à l'inconnu
+                        if (value == inconnu) {
+                            outn("Bravo le nombre était : " + inconnu + " !");
+                            isValueCorrect = true;
+                            // Inrémenter le passage de niveau
+                            level++;
+                        } else {
+                            chance--;
+                            if (value > inconnu) {
+                                out("Valeur fournie est trop grande !");
+                            } else if (value < inconnu) {
+                                out("Valeur fournie est trop petite !");
+                            }
+                            outn("Il vous reste " + chance + " chances");
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Vous avez perdu vous êtes informé
+             */
+            if (chance <= 0) {
+                outn("Désoler, vous avez perdu ! Vous n'avez plus de chance.");
+            }
+
+            /**
+             * C'est recommencer depuis le début our arrêter
+             */
+            yesno = "";
+            while (!yesno.matches("y") && !yesno.matches("n")) {
+                outn("Voulez-vous recommencer ? y/n");
+                try {
+                    yesno = scan.next();
+                } catch (NoSuchElementException e) {
+                    outn("Merci de specifier une information correct ! ");
+                    yesno = "";
+                }
+                yesno = yesno.toLowerCase();
+                outn("YesNo tolowerCase :" + yesno + " >> " + yesno.toLowerCase());
+            }
+        }
+        outn("Merci d'avoir participer !");
+
+    }
+
+    /**
+     *
+     * Options : chance avec limitation d'essai / Limiter le nombre d'essai
+     * simultanée !
+     *
+     * Options : Niveau difficulté Niveau 0 - 0...10 Niveau 1 - 0...20 Niveau 2
+     * - 0...30 ...
+     *
+     * Options : Calculer le score de la personne Nombre pour atteindre un
+     * niveau
+     *
+     *
      */
     public static void trouveLeNombreAleatoire() {
         String yesno = "y";
 
         while (yesno.matches("y")) {
-            out("=|=====================================================|=");
-            out("=| Trouver un nombre ent 0 et n                        |=");
-            out("=|=====================================================|=");
+            outn("=|=====================================================|=");
+            outn("=| Trouver un nombre ent 0 et n                        |=");
+            outn("=|=====================================================|=");
 
             Integer inconnu = (int) (Math.random() * 10);
-            out("Essayer de retrouver le nombre caché compris entre 0 et 10 :");
+            outn("Essayer de retrouver le nombre caché compris entre 0 et 10 :");
             Scanner scan = new Scanner(System.in);
 
             Boolean isValueCorrect = false;
@@ -66,49 +161,65 @@ public class TP {
                 try {
                     value = scan.nextInt();
                 } catch (InputMismatchException erreur) {
-                    out("Valeur renseignée ne peut être convertie"
+                    outn("Valeur renseignée ne peut être convertie"
                             + "\nErreur : " + erreur.getMessage());
                     return;
                 }
                 // Compare si le nombre fourni par l'utilisateur correspond à l'inconnu
                 if (value > inconnu) {
-                    out("Valeur fournie est trop grand !");
+                    outn("Valeur fournie est trop grand !");
                 } else if (value < inconnu) {
-                    out("Valeur fournie est trop petite ! ");
+                    outn("Valeur fournie est trop petite ! ");
                 } else {
-                    out("Bravo le nombre était : " + inconnu + " !");
+                    outn("Bravo le nombre était : " + inconnu + " !");
                     isValueCorrect = true;
                 }
             }
 
             yesno = "";
             while (!yesno.matches("y") && !yesno.matches("n")) {
-                out("Voulez-vous recommencer ? y/n");
-                try{
+                outn("Voulez-vous recommencer ? y/n");
+                try {
                     yesno = scan.next();
-                }catch(NoSuchElementException  e){
-                    out("Merci de specifier une information correct ! ");
+                } catch (NoSuchElementException e) {
+                    outn("Merci de specifier une information correct ! ");
                     yesno = "";
                 }
                 yesno = yesno.toLowerCase();
-                out("YesNo tolowerCase :" + yesno + " >> " + yesno.toLowerCase());
+                outn("YesNo tolowerCase :" + yesno + " >> " + yesno.toLowerCase());
             }
         }
-        out("Merci d'avoir participer !");
+        outn("Merci d'avoir participer !");
 
+    }
+
+    public static void boucleWhileDo() {
+        Integer A = 500;
+        Integer B = 450;
+        Integer i = 1;
+
+        while (B < A) {
+            outn(i + " : " + B + " < " + A + " : ");
+            B++; // incrémentation B de 1
+            i++;
+        }
+
+//        do {
+//
+//        } while (B < A);
     }
 
     public static void mainFactoriel() {
         Scanner scan = new Scanner(System.in);
         Integer value = 0;
-        out("Insérer une valeur :");
+        outn("Insérer une valeur :");
         try {
             value = Integer.valueOf(scan.nextInt());
         } catch (NumberFormatException e) {
-            out("Valeur de nombre attendu : " + e.getMessage());
+            outn("Valeur de nombre attendu : " + e.getMessage());
             return;
         }
-        out(value + "!=" + factoriel(value));
+        outn(value + "!=" + factoriel(value));
 
     }
 
@@ -125,13 +236,17 @@ public class TP {
         Integer value = n;
         while (i < n) {
             value = value * (n - i);
-            out(i + " :" + value);
+            outn(i + " :" + value);
             i++;
         }
         return value;
     }
 
-    public static void out(String msg) {
+    public static void outn(String msg) {
         System.out.println(msg);
+    }
+
+    public static void out(String msg) {
+        System.out.print(msg);
     }
 }
